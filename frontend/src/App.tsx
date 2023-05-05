57e4ed93c261bc5d130f645bcf4300d0be09001c
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-05-05 16:49:47 +0530

    [Frontend]. Initial setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import classes from './app.module.css';
+
+const App = () => {
+    return (
+        <div className={classes.container}>
+            <h1>Hello</h1>
+        </div>
+    );
+};
+
+export default App;
