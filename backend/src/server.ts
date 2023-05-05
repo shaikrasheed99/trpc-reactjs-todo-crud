aa3b16c9deb9efe24bf171e64440c46c86d52d75
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-05-05 22:26:16 +0530

    [Backend]. TRPC test api

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,15 @@
 import express from 'express';
+import * as trpcExpress from '@trpc/server/adapters/express';
+import { trpcRouter } from './router';
 
 const app = express();
+
+app.use(
+    '/trpc',
+    trpcExpress.createExpressMiddleware({
+        router: trpcRouter,
+    }),
+);
 
 app.get('/test', (req, res) => {
     res.send('This is test api!!');
