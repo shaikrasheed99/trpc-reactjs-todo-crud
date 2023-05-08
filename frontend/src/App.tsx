84689c6b3db8b713cd817a4f8547b80d6f40be2b
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-05-08 17:49:22 +0530

    Add. Integrated get todos api with frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,8 @@
 import classes from './app.module.css';
 import Navbar from './components/layouts/navbar/Navbar';
 import Todos from './components/pages/todos/Todos';
-import { ITodo, Priority } from './components/pages/types';
 import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
 import type { TRPCRouter } from '../../backend/src/router';
-import { useState } from 'react';
 
 const TRPC_ENDPOINT = 'http://localhost:3000/trpc/';
 
@@ -16,52 +14,11 @@ export const trpc = createTRPCProxyClient<TRPCRouter>({
     ],
 });
 
-const defaultTodos: ITodo[] = [
-    {
-        id: 1,
-        description: 'build todo trpc application',
-        priority: Priority.HIGH,
-        isCompleted: false,
-    },
-    {
-        id: 2,
-        description: 'build todo trpc frontend application',
-        priority: Priority.MEDIUM,
-        isCompleted: false,
-    },
-    {
-        id: 3,
-        description: 'build todo trpc frontend application in typescript',
-        priority: Priority.LOW,
-        isCompleted: false,
-    },
-    {
-        id: 4,
-        description: 'build todo trpc application',
-        priority: Priority.HIGH,
-        isCompleted: false,
-    },
-    {
-        id: 5,
-        description: 'build todo trpc frontend application',
-        priority: Priority.MEDIUM,
-        isCompleted: false,
-    },
-    {
-        id: 6,
-        description: 'build todo trpc frontend application in typescript',
-        priority: Priority.LOW,
-        isCompleted: false,
-    },
-];
-
 const App = () => {
-    const [todos, setTodos] = useState<ITodo[]>(defaultTodos);
-
     return (
         <div className={classes.container}>
             <Navbar />
-            <Todos todos={todos} setTodos={setTodos} />
+            <Todos />
         </div>
     );
 };
--- frontend/src/components/pages/todos/Todos.tsx
+++ frontend/src/components/pages/todos/Todos.tsx
@@ -2,19 +2,28 @@ import classes from './todos.module.css';
 import Todo from '../todo/Todo';
 import EmptyTodos from '../emptyTodos/EmptyTodos';
 import { ITodo } from '../types';
+import { useState, useEffect } from 'react';
+import { trpc } from '../../../App';
 
-interface TodosProp {
-    todos: ITodo[];
-    setTodos: (todos: ITodo[]) => void;
-}
+const Todos = () => {
+    const [todos, setTodos] = useState<ITodo[]>([]);
 
-const Todos = ({ todos, setTodos }: TodosProp) => {
+    useEffect(() => {
+        const todosFromServer = async () => {
+            const todos = await trpc.getTodos.query();
+
+            setTodos(todos as ITodo[]);
+        };
+
+        todosFromServer();
+    });
+
     const isEmpty = (todos: ITodo[]) => {
         return todos.length === 0;
     };
 
     const handleDelete = (id: number) => {
-        const filteredTodos = todos.filter((todo) => todo.id != id);
+        const filteredTodos = todos.filter((todo: ITodo) => todo.id != id);
         setTodos(filteredTodos);
     };
 
